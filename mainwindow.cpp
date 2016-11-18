f826c013f98d48fd4d1ec033aed06d811f629acf
Artem <mail@artempal.ru>
2016-11-18 19:55:17 +0300

    вывод пар в главном окне

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,8 +7,20 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     marks_select();
     schedule_show();
+    QString name_day[] = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота"};
+    QString h1_text = "Сегодня";
+
+    h1_text.append(", ");
+    h1_text.append(current_date.toString("d MMM yyyy"));
+    h1_text.append(", ");
+    h1_text.append(name_day[day_week-1]);
+    h1_text.append(", идет ");
+    h1_text.append(QString::number(cur_week));
+    h1_text.append("-ая неделя учебы");
+    ui->h1_main->setText(h1_text);
 
     ui->form_save_lable->hide();
     ui->dateEdit->setDate(current_date);
@@ -24,7 +36,6 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QPixmap myPixmap("C:/qtprojects/curs/course_2016.git/tem2.jpg"); //фотография
     ui->photo->setPixmap(myPixmap);
-
 }
 
 MainWindow::~MainWindow()
@@ -68,35 +79,45 @@ void MainWindow::marks_select()
          marks_text.append(a_query.value(res.indexOf("text")).toString());
      }
 
-     if(marks_text == "") marks_text = "Заметок на сегодня нет!";
+     //if(marks_text == "") marks_text = "Заметок на сегодня нет!";
 
      ui->marks_conteiner->setText(marks_text);
 }
 
-void MainWindow::sch_select()
+void MainWindow::sch_select(int day_week, int week)
 {
+     int type_week = 1;
+     if(week % 2 == 0) type_week = 2;
      if(!_db_connect)db_connect(); //проверка подключения к базе
-     marks_text = ""; // обнуляем переменную
+     sch_text = ""; // обнуляем переменную
 
      QSqlQuery a_query; // переменная для запроса
+     QString str_select; //текст запроса
 
-     if (!a_query.exec("SELECT * FROM schedule WHERE day = %1")) //тут я остановился
+     if(type_week == 2) str_select = "SELECT * FROM schedule WHERE day = %1 LIMIT 5";
+     if(type_week == 1) str_select = "SELECT * FROM schedule WHERE day = %1 LIMIT 5,5";
+     QString str = str_select.arg(day_week);
+
+
+     if (!a_query.exec(str))
      {
              qDebug() << "Ошибка выполнения запроса SELECT";
      }
 
 
      QSqlRecord res = a_query.record(); //результат запроса
-
+    int i = 1; //счетчик пар
      while (a_query.next())
      {
-         marks_text.append("\n\n"); //отступ от заметок
-         marks_text.append(a_query.value(res.indexOf("text")).toString());
+         sch_text.append(QString::number(i));
+         sch_text.append(". ");
+         QString para = a_query.value(res.indexOf("name")).toString();
+         if(para == "") sch_text.append("-"); else sch_text.append(para);
+         sch_text.append("\n\n");
+         i++;
      }
 
-     if(marks_text == "") marks_text = "Заметок на сегодня нет!";
-
-     ui->marks_conteiner->setText(marks_text);
+     if(sch_text == "") sch_text = "Ура! Cегодня пар нет!";
 }
 
 
@@ -156,9 +177,9 @@ void MainWindow::open_sch()
 }
 void MainWindow::schedule_show()
 {
-    int start_week = 35;
-    int week = current_date.weekNumber();
-    int cur_week = week - start_week + 1;
-    ui->sch_output->setText("Расписанка");
-    qDebug() << cur_week;
+
+
+    sch_select(day_week,cur_week);
+
+    ui->sch_output->setText(sch_text);
 }
