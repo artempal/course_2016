29fdff738a7f451b85994d873b2df615abfba102
Artem <mail@artempal.ru>
2016-11-18 19:56:18 +0300

    вынос глобальных переменных даты и редактирование функции вывода расписания

--- mainwindow.h
+++ mainwindow.h
@@ -29,13 +29,18 @@ private:
     bool _db_connect = 0;
     Ui::MainWindow *ui;
     QString marks_text; //вывод заметок
+    QString sch_text; //вывод расписания
     QString mark = ""; //заметка из формы
+    const int start_week = 35; //неделя начала занятий
     QDate current_date = QDate::currentDate(); //текущяя дата
+    int week = current_date.weekNumber(); //номер недели
+    int day_week = current_date.dayOfWeek(); //номер дня недели (1-7)
+    int cur_week = week - start_week + 1; //неделя в расписании МИРЭА
     QString form_date; //дата из формы
     void db_connect(); // функция подключения к базе
     void marks_select(); //функция выборки заметок
     void schedule_show(); //функция показа расписания
-    void sch_select(); //выборка расписания из базы
+    void sch_select(int day_week, int week); //выборка расписания из базы
 private slots:
     void send_form();
     void open_sch();
